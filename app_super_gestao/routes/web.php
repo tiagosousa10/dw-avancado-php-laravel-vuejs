43e4628b6934a2f6a686a46c7e830655832bb21f
Tiago Sousa <tiagosousa.tams@hotmail.com>
2025-11-02 18:22:44 +0000

    attribuing a name for routes

--- app_super_gestao/routes/web.php
+++ app_super_gestao/routes/web.php
@@ -13,21 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/", 'PrincipalController@principal');
-
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
-
-Route::get('/contato', 'ContatoController@contato');
-
-Route::get('/login', function() {return 'Login';});
+Route::get("/", 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contato')->name('site.contato');
+Route::get('/login', function() {return 'Login';})->name('site.login');
 
 //app
 Route::prefix('/app')->group(function() {
-Route::get('/clientes', function() {return 'clientes';});
+Route::get('/clientes', function() {return 'clientes';})->name('app.clientes');
 Route::get('/fornecedores', function() {
     return 'fornecedores';
-});
-Route::get('/produtos', function() {return 'produtos';});
+})->name(('app.fornecedores'));
+Route::get('/produtos', function() {return 'produtos';})->name('app.produtos');
 });
 
 
